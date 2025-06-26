d9657966d3c2da591e44ba2f363ae509856ef067
nicollasemjy <nicollasemjy@gmail.com>
2025-06-26 14:04:35 -0300

    Build local COMPILADO COM SUCESSO. Pronto para deploy final com estilos!

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Certifique-se de que este caminho está correto
   ],
   theme: {
     extend: {},
